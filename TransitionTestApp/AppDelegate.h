b9c718f901e45e357dc07d7124207cbf32bd090a
Uli Kusterer <ulivc@zathras.de>
2025-11-05 01:21:24 +0100

    Initial Commit

---
+++ TransitionTestApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TransitionTestApp
+//
+//  Created by Uli Kusterer on 05.11.25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TransitionTestApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  TransitionTestApp
+//
+//  Created by Uli Kusterer on 05.11.25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // Setup code that might create autoreleased objects goes here.
+	}
+	return NSApplicationMain(argc, argv);
+}
